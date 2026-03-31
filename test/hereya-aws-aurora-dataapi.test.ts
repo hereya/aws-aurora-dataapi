d9ec388ec85f1d11cae89ddefa59173b50e2b870
Jonatan Sombié <jonatan.sombie@gmail.com>
2026-03-31 10:48:36 +0200

    Initial commit

---
+++ test/hereya-aws-aurora-dataapi.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HereyaAwsAuroraDataapi from '../lib/hereya-aws-aurora-dataapi-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hereya-aws-aurora-dataapi-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HereyaAwsAuroraDataapi.HereyaAwsAuroraDataapiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
